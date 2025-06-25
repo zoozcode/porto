f2af5c5b19b5ed28eb8ec3474cb44289ac28bd7b
zooz.code <ibnu19173@gmail.com>
2025-06-26 00:34:14 +0700

    Update porto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>portofolio</title>
     <link rel="icon" type="image/x-icon" href="zooz.png">
     <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
